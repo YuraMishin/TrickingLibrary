89f60a6e6a6c4889f8568dc7f5b35e6225c6eceb
Mishin Yura <mishin.inbox@gmail.com>
2021-04-02 15:12:43 +0300

    Displayed a message via asyncData. Closes #4

--- src/web-nuxt/pages/index.vue
+++ src/web-nuxt/pages/index.vue
@@ -1,15 +1,25 @@
 <template>
   <div>
-    {{message}}
+    <p>{{message}}</p>
+    <p>{{messageAsyncData}}</p>
   </div>
 </template>
 
 <script>
+  import Axios from "axios";
 
   export default {
     components: {},
     data: () => ({
-      message: "Hello World!"
-    })
+      message: "Hello World basic!",
+      messageAsyncData: "Hello World asyncData!"
+    }),
+    asyncData(payload){
+      console.log("asyncData called")
+      return Axios.get("http://localhost:5000/api/home")
+        .then(({data}) => {
+          return { messageAsyncData: data }
+        })
+    }
   }
 </script>
