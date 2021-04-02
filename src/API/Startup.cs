a130bd22d3a95699038b9ffb257f38998d491344
Mishin Yura <mishin.inbox@gmail.com>
2021-04-02 13:59:23 +0300

    Displayed Hello World via Nuxt UI. Closes #3

--- src/API/Startup.cs
+++ src/API/Startup.cs
@@ -1,40 +1,67 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
 namespace API
 {
-    public class Startup
+  public class Startup
+  {
+    private readonly IConfiguration _configuration;
+    private readonly IWebHostEnvironment _env;
+    private const string AllCors = "All";
+
+    public Startup(IConfiguration configuration, IWebHostEnvironment env)
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services)
-        {
-        }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
-            app.UseRouting();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
-            });
-        }
+      _configuration = configuration;
+      _env = env;
     }
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+      services.AddControllers();
+
+      #region Dependencies Injection
+
+      #endregion
+
+      services.AddCors(options => options.AddPolicy(AllCors, build => build.AllowAnyHeader()
+        .AllowAnyOrigin()
+        .AllowAnyMethod()));
+    }
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app, ILogger<Startup> logger)
+    {
+      logger.LogInformation("App is running. Enjoy!");
+
+      if (_env.IsDevelopment())
+      {
+        logger.LogInformation("Development mode");
+        app.UseDeveloperExceptionPage();
+      }
+
+      if (_env.IsProduction())
+      {
+        logger.LogInformation("Production mode");
+        app.UseDeveloperExceptionPage();
+      }
+
+      app.UseCors(AllCors);
+
+      app.UseRouting();
+
+      app.UseEndpoints(endpoints =>
+      {
+        endpoints.MapDefaultControllerRoute();
+        endpoints.MapGet("/test", async context =>
+        {
+          await context.Response.WriteAsync("Hello World!");
+        });
+      });
+    }
+  }
 }
