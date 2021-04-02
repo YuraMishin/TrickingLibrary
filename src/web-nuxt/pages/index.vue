feff370fbec608b69b7a6865b5f44fb0e65aef0c
Mishin Yura <mishin.inbox@gmail.com>
2021-04-02 15:32:31 +0300

    Displayed a message via Vuex. Closes #5

--- src/web-nuxt/pages/index.vue
+++ src/web-nuxt/pages/index.vue
@@ -2,11 +2,13 @@
   <div>
     <p>{{message}}</p>
     <p>{{messageAsyncData}}</p>
+    <p>{{messageVuex}}</p>
   </div>
 </template>
 
 <script>
   import Axios from "axios";
+  import {mapActions, mapMutations, mapState} from 'vuex';
 
   export default {
     components: {},
@@ -14,12 +16,18 @@
       message: "Hello World basic!",
       messageAsyncData: "Hello World asyncData!"
     }),
+    computed: {
+      ...mapState(['messageVuex'])
+    },
     asyncData(payload){
       console.log("asyncData called")
       return Axios.get("http://localhost:5000/api/home")
         .then(({data}) => {
           return { messageAsyncData: data }
         })
+    },
+    async fetch (){
+      await this.$store.dispatch('fetchMessage');
     }
   }
 </script>
---
+++ src/web-nuxt/store/index.js
@@ -0,0 +1,31 @@
+﻿const initState = () => ({
+  messageNuxtServerInit: "init",
+  messageVuex: "init"
+})
+
+export const state = initState
+
+export const mutations = {
+  setMessageNuxtServerInit(state, message) {
+    state.messageNuxtServerInit = message
+  },
+  setMessageVuex(state, message) {
+    state.messageVuex = message
+  },
+  reset(state) {
+    Object.assign(state, initState())
+  }
+}
+
+export const actions = {
+  async nuxtServerInit({commit, dispatch}) {
+    console.log('nuxtServerInit called');
+    const message = 'Hello world from nuxtServerInit()';
+    commit("setMessageNuxtServerInit", message)
+  },
+
+  async fetchMessage({commit}) {
+    const message = 'Hello world from vuex';
+    commit('setMessageVuex', message);
+  }
+}
