fa5a0b2a65bff39a7400145f460144cde4665211
Mishin Yura <mishin.inbox@gmail.com>
2021-04-03 11:52:13 +0300

    Edited nuxt.config.js. Useed axios base url option. Closes #13

--- src/web-nuxt/store/videos.js
+++ src/web-nuxt/store/videos.js
@@ -15,7 +15,7 @@ export const mutations = {
 
 export const actions = {
   startVideoUpload({commit, dispatch}, {form}){
-     const uploadPromise = this.$axios.$post("http://localhost:5000/api/videos", form);
+     const uploadPromise = this.$axios.$post("/api/videos", form);
       commit("setTask", {uploadPromise})
   }
 }
