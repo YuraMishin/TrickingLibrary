479ed04547bb670cc5792ba1e6ac24f97c8e519e
Mishin Yura <mishin.inbox@gmail.com>
2021-04-04 16:38:47 +0300

    Split trick & submission UI. Closes #19

--- src/web-nuxt/store/tricks.js
+++ src/web-nuxt/store/tricks.js
@@ -4,18 +4,28 @@
 
 export const state = initState
 
+export const getters = {
+  trickItems: state => state.tricks.map(x => ({
+    text: x.name,
+    value: x.id
+  }))
+}
+
 export const mutations = {
   setTricks(state, {tricks}) {
-    state.tricks = tricks;
+    state.tricks = tricks
   },
-  reset(state){
-    Object.assign(state, initState());
+  reset(state) {
+    Object.assign(state, initState())
   }
 }
 
 export const actions = {
-  async fetchTricks({commit}){
+  async fetchTricks({commit}) {
     const tricks = await this.$axios.$get("/api/tricks");
-    commit("setTricks", {tricks});
+    commit("setTricks", {tricks})
+  },
+  createTrick({state, commit, dispatch}, {form}) {
+    return this.$axios.$post("/api/tricks", form)
   }
 }
