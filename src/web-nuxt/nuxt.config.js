79cd06cd324dae067eb6d5ce7d82ed0414868ef6
Mishin Yura <mishin.inbox@gmail.com>
2021-04-09 09:16:54 +0300

    Enhanced specific trick page. Closes #24

--- src/web-nuxt/nuxt.config.js
+++ src/web-nuxt/nuxt.config.js
@@ -3,7 +3,7 @@ import colors from 'vuetify/es5/util/colors'
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    titleTemplate: '%s - web-nuxt',
+    titleTemplate: '%s - Tricking Library',
     title: 'web-nuxt',
     htmlAttrs: {
       lang: 'en'
